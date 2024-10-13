f7b3adb95e972c60a451d618e4b322c5af80f238
zhoukangkai <719296667@qq.com>
2024-10-13 19:29:49 +0800

    update: Remove DataSourceAutoConfiguration import due to unnecessary auto-configuration.

--- src/main/java/com/nus/zkk/bodhifiles_system/BodhiFilesSystemApplication.java
+++ src/main/java/com/nus/zkk/bodhifiles_system/BodhiFilesSystemApplication.java
@@ -2,7 +2,6 @@ package com.nus.zkk.bodhifiles_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.nus.zkk.bodhifiles_system.config")
