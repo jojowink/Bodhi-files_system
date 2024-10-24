c216986ef2a2fb131bed10e1567d6b3332c62353
zhoukangkai <719296667@qq.com>
2024-10-24 20:52:07 +0800

    update: Updated file records, added new files, corrected paths.

--- sql脚本/bodhi-files_system.sql
+++ sql脚本/bodhi-files_system.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 23/10/2024 16:44:47
+ Date: 24/10/2024 20:50:46
 */
 
 SET NAMES utf8mb4;
@@ -44,7 +44,7 @@ CREATE TABLE `file_info`  (
   `path` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件或文件夹的路径',
   `absolute_path` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件或文件夹的绝对路径',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '文件资源表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '文件资源表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of file_info
@@ -54,6 +54,6 @@ INSERT INTO `file_info` VALUES (2, 1, '17-无人机相关组件-1140.jpg', 1, 'f
 INSERT INTO `file_info` VALUES (3, 1, '子文件夹', 1, 'folder', NULL, NULL, -1, -1, b'0', b'0', b'1', NULL, '2024-10-22 16:39:27', 'jojo', '2024-10-22 16:39:33', 'jojo', '2024-10-22 16:39:41', NULL, '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹\', \'Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹', '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹\', \'Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹');
 INSERT INTO `file_info` VALUES (4, 3, '子文件.jpg', NULL, 'file', 'www.baidu.com/1', '.jpg', -1, -1, b'0', b'1', b'0', 512, '2024-10-22 16:41:12', 'jojo', '2024-10-22 16:41:18', 'jojo', '2024-10-22 16:41:24', NULL, '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹/子文件.jpg', '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/子文件夹/子文件.jpg');
 INSERT INTO `file_info` VALUES (5, 1, 'CPP设计-C高阶.md\r\n', 1, 'file', 'https://bodhi-filessystem.oss-ap-southeast-1.aliyuncs.com/CPP%E8%AE%BE%E8%AE%A1-C%E9%AB%98%E9%98%B6.md', '.md', -1, -1, b'1', b'0', b'0', NULL, '2024-10-23 16:15:23', 'jojo', '2024-10-23 16:15:29', 'jojo', '2024-10-23 16:15:34', NULL, '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-C高阶.md\r\n', '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-C高阶.md\r\n');
-INSERT INTO `file_info` VALUES (6, 1, 'CPP设计-JSON.md', 1, 'file', 'https://bodhi-filessystem.oss-ap-southeast-1.aliyuncs.com/CPP%E8%AE%BE%E8%AE%A1-JSON.md', '.md', -1, -1, b'1', b'0', b'0', NULL, '2024-10-23 16:17:20', 'jojo', '2024-10-23 16:17:27', 'jojo', '2024-10-23 16:17:32', NULL, '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140//Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-C高阶.md\r\n', '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140//Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-C高阶.md\r\n');
+INSERT INTO `file_info` VALUES (6, 1, 'CPP设计-JSON.md', 1, 'file', 'https://bodhi-filessystem.oss-ap-southeast-1.aliyuncs.com/CPP%E8%AE%BE%E8%AE%A1-JSON.md', '.md', -1, -1, b'1', b'0', b'0', NULL, '2024-10-23 16:17:20', 'jojo', '2024-10-23 16:17:27', 'jojo', '2024-10-23 16:17:32', NULL, '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140//Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-JSON.md\r\n', '/Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140//Users/cheng-xuanzhu/Downloads/17-无人机相关组件-1140/CPP设计-JSON.md');
 
 SET FOREIGN_KEY_CHECKS = 1;
